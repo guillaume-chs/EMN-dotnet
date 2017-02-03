fb34d2d74ffa8008c5dcbbfddb40ae6fee566955
kepotx <kepa-bourgoin@hotmail.fr>
2017-02-03 11:46:57 +0100

    refactor Hotels DAL

--- PKG-vols-hotels/DALHotels/Hotels.cs
+++ PKG-vols-hotels/DALHotels/Hotels.cs
@@ -10,75 +10,65 @@ namespace DALHotels
 {
     public class Hotels
     {
-        String TABLE_NAME = "HOTELS";
-        String DB_NAME = "(localdb)";
-        public DataSet GetHotels()
+        public const string SERVER_NAME = "KEPA-PC\\SQLEXPRESS";
+        public const string DB_NAME = "HOTELS";
+        public const string TABLE_NAME = "HOTELS";
+        private DataSet callSP(SqlCommand command)
         {
-            SqlConnection connection = new SqlConnection();
-            connection.ConnectionString = "Data Source=" + DB_NAME + ";Initial Catalog=" + TABLE_NAME + ";Integrated Security = true";
-            connection.Open();
-
-            SqlCommand command = new SqlCommand("sp_getHotels", connection);
-            command.CommandType = CommandType.StoredProcedure;
-
             DataSet result = new DataSet();
 
-            SqlDataAdapter sda = new SqlDataAdapter();
-            sda.SelectCommand = command;
+            try
+            {
+                SqlConnection connection = new SqlConnection();
+                connection.ConnectionString = "Data Source=" + SERVER_NAME + ";Initial Catalog=" + DB_NAME + ";Integrated Security = true";
+                connection.Open();
 
-            sda.Fill(result, TABLE_NAME);
+                command.Connection = connection;
 
-            sda.Dispose();
-            command.Dispose();
-            connection.Close();
+                SqlDataAdapter sda = new SqlDataAdapter();
+                sda.SelectCommand = command;
+
+                sda.Fill(result, TABLE_NAME);
+
+                sda.Dispose();
+                command.Dispose();
+                connection.Close();
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.Message);
+            }
 
             return result;
+        }
+
+        public DataSet GetHotels()
+        {
+            SqlCommand command = new SqlCommand("sp_getHotels");
+            command.CommandType = CommandType.StoredProcedure;
+
+            return callSP(command);
+
         }
         public DataSet GetHotelById(int id)
         {
-            SqlConnection connection = new SqlConnection();
-            connection.ConnectionString = "Data Source=" + DB_NAME + ";Initial Catalog=" + TABLE_NAME + ";Integrated Security = true";
-            connection.Open();
-
-            SqlCommand command = new SqlCommand("sp_getHotelById", connection);
+            SqlCommand command = new SqlCommand("sp_getHotelById");
             command.CommandType = CommandType.StoredProcedure;
-            command.Parameters.Add("@ID", SqlDbType.Int, sizeof(int));
+
+            command.Parameters.Add("@ID", SqlDbType.Int);
             command.Parameters["@ID"].Value = id;
-            DataSet result = new DataSet();
 
-            SqlDataAdapter sda = new SqlDataAdapter();
-            sda.SelectCommand = command;
-
-            sda.Fill(result, TABLE_NAME);
-
-            sda.Dispose();
-            command.Dispose();
-            connection.Close();
-
-            return result;
+            return callSP(command);
         }
         public DataSet GetHotelByCity(String city)
         {
-            SqlConnection connection = new SqlConnection();
-            connection.ConnectionString = "Data Source=" + DB_NAME + ";Initial Catalog=" + TABLE_NAME + ";Integrated Security = true";
-            connection.Open();
-
-            SqlCommand command = new SqlCommand("sp_getHotelByCity", connection);
+            SqlCommand command = new SqlCommand("sp_getHotelByCity");
             command.CommandType = CommandType.StoredProcedure;
+
             command.Parameters.Add("@CITY", SqlDbType.VarChar, 50);
             command.Parameters["@CITY"].Value = city;
-            DataSet result = new DataSet();
-
-            SqlDataAdapter sda = new SqlDataAdapter();
-            sda.SelectCommand = command;
-
-            sda.Fill(result, TABLE_NAME);
-
-            sda.Dispose();
-            command.Dispose();
-            connection.Close();
-
-            return result;
+            return callSP(command);
+            
         }
     }
 
