770b959c98ed85daf9700da42fea347d72f693d1
Paul Defois <defois.p@laposte.net>
2017-02-03 13:40:13 +0100

    Ajout BL Vol

--- PKG-vols-hotels/DALVols/Vols.cs
+++ PKG-vols-hotels/DALVols/Vols.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Data;
 using System.Data.SqlClient;
 
-namespace DALVols
+namespace ResaVoyages.DALVols
 {
     public class Vols
     {
