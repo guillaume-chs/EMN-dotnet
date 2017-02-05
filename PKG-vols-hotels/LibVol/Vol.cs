44e56ee1f0dcccc96ec2fac3588ad3c2007283bd
Paul Defois <defois.p@laposte.net>
2017-02-05 17:58:36 +0100

    Modif libvol

--- PKG-vols-hotels/LibVol/Vol.cs
+++ PKG-vols-hotels/LibVol/Vol.cs
@@ -51,24 +51,35 @@ namespace ResaVoyages.BL.LibVol
             new Vol(0, name, departureDate, arrivalDate, departureCity, arrivalCity, price, capacity);
         }
 
-        public static List<Vol> DataSetToVols(DataSet dataSet)
+        protected static List<Vol> DataSetToVols(DataSet dataSet)
         {
             List<Vol> vols = new List<Vol>();
 
-            foreach (DataRow vol in dataSet.Tables[DALVols.TABLE_NAME].Rows)
+            if (dataSet.Tables.Count == 1 && dataSet.Tables.Contains(DALVols.TABLE_NAME))
             {
-                object[] cols = vol.ItemArray;
-                vols.Add(new Vol(
-                    Convert.ToInt32(cols[0]),
-                    Convert.ToString(cols[1]),
-                    Convert.ToDateTime(cols[2]),
-                    Convert.ToDateTime(cols[3]),
-                    Convert.ToString(cols[4]),
-                    Convert.ToString(cols[5]),
-                    Convert.ToSingle(cols[6]),
-                    Convert.ToInt32(cols[7])
-                ));
+                foreach (DataRow vol in dataSet.Tables[DALVols.TABLE_NAME].Rows)
+                {
+                    object[] cols = vol.ItemArray;
+                    vols.Add(new Vol(
+                        Convert.ToInt32(cols[0]),
+                        Convert.ToString(cols[1]),
+                        Convert.ToDateTime(cols[2]),
+                        Convert.ToDateTime(cols[3]),
+                        Convert.ToString(cols[4]),
+                        Convert.ToString(cols[5]),
+                        Convert.ToSingle(cols[6]),
+                        Convert.ToInt32(cols[7])
+                    ));
+                }
             }
+
+            return vols;
+        }
+
+        public static List<Vol> GetVols(DateTime departureDate, String departureCity, String arrivalCity)
+        {
+            DALVols dalVols = new DALVols();
+            List<Vol> vols = DataSetToVols(dalVols.GetVolsByDepartureDateDepartureCityArrivalCity(departureDate, departureCity, arrivalCity));
 
             return vols;
         }
@@ -76,7 +87,7 @@ namespace ResaVoyages.BL.LibVol
         /**
          * Renvoie la liste des couples (ville de départ, villes d'arrivée)
          */
-        public static Dictionary<string, List<string>> getCities()
+        public static Dictionary<string, List<string>> GetCities()
         {
             DALVols dalVols = new DALVols();
             List<Vol> vols = DataSetToVols(dalVols.GetVols());
