4a7f2bd301055c425c228b6d5cd919fef02e6f6c
Polo <defois.p@laposte.net>
2017-02-07 19:01:15 +0100

    Ajout form

--- PKG-vols-hotels/FileReservationmanager/Form1.cs
+++ PKG-vols-hotels/FileReservationmanager/Form1.cs
@@ -16,5 +16,15 @@ namespace FileReservationmanager
         {
             InitializeComponent();
         }
+
+        private void ajoutFile_click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lireFile_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
