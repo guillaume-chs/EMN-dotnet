bc66397e92f23319d6ec23447bdee70206fc418c
kepotx <kepa-bourgoin@hotmail.fr>
2017-02-07 18:08:08 +0100

    Update DalHotel/LibHotel

--- PKG-vols-hotels/LibHotel/Hotel.cs
+++ PKG-vols-hotels/LibHotel/Hotel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ResaVoyages.DAL.DALHotels;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.Text;
@@ -40,5 +42,51 @@ namespace LibHotel
         {
             new Hotel(0, name, city, price, capacity);
         }
+
+         protected static List<Hotel> DataSetToHotels(DataSet dataSet)
+        {
+            List<Hotel> hotels = new List<Hotel>();
+
+            if (dataSet.Tables.Count == 1 && dataSet.Tables.Contains(DALHotels.TABLE_NAME))
+            {
+                foreach (DataRow hotel in dataSet.Tables[DALHotels.TABLE_NAME].Rows)
+                {
+                    object[] cols = hotel.ItemArray;
+                    hotels.Add(new Hotel(
+                        Convert.ToInt32(cols[0]),
+                        Convert.ToString(cols[1]),
+                        Convert.ToString(cols[2]),
+                        Convert.ToSingle(cols[3]),
+                        Convert.ToInt32(cols[4])
+                    ));
+                }
+            }
+
+            return hotels;
+        }
+
+        public static List<Hotel> GetHotelsByCity(String city)
+        {
+            DALHotels dalHotels = new DALHotels();
+            List<Hotel> hotels = DataSetToHotels(dalHotels.GetHotelsByCity(city));
+
+            return hotels;
+        }
+
+
+        public static void reserver(int idHotel, int nbRooms, string nom, string prenom)
+        {
+            DALHotels dalHotels = new DALHotels();
+
+            if (Convert.ToInt32(dalHotels.GetHotelCapacityById(idHotel).Tables[0].Rows[0]["CAPACITY"].ToString()) > nbRooms)
+            {
+                dalHotels.DecrementHotelSeatsById(idHotel, nbRooms);
+
+                //A mettre dans une MSMQ
+                dalHotels.InsertReservationHotel(nom, prenom, nbRooms, idHotel);
+            }
+
+        }
+
     }
 }
